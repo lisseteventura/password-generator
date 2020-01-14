f41d3d6046f707f12ee797ca35116534bc8a8ad7
lisseteventura <lisseteventura@gmail.com>
2020-01-14 15:20:30 -0500

    generator functions properly

--- main.js
+++ main.js
@@ -4,12 +4,20 @@ const form = document.getElementById("passwordGeneratorForm");
 const includeUppercaseElement = document.getElementById("includeUppercase");
 const includeNumbersElement = document.getElementById("includeNumbers");
 const includeSymbolsElement = document.getElementById("includeSymbols");
+const passwordDisplay = document.getElementById("pwDisplay");
+
+const uppercase_char_codes = arrayFromLowToHigh(65, 90);
+const lowercase_char_codes = arrayFromLowToHigh(97, 122);
+const numbers_char_codes = arrayFromLowToHigh(48, 57);
+const symbols_char_codes = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
 
 //everytime the input changes, the sync function will be called
 characterAmountNumber.addEventListener("input", syncCharacterAmount);
 characterAmountRange.addEventListener("input", syncCharacterAmount);
 
-//everytime we submit we want the page to prevent from refreshing
 form.addEventListener("submit", e => {
   e.preventDefault();
   const characterAmount = characterAmountNumber.value;
@@ -22,7 +30,36 @@ form.addEventListener("submit", e => {
     includeNumbers,
     includeSymbols
   );
+  passwordDisplay.innerText = password;
 });
+
+function generatePassword(
+  characterAmount,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  let charCodes = lowercase_char_codes;
+  if (includeUppercase) charCodes = charCodes.concat(uppercase_char_codes);
+  if (includeNumbers) charCodes = charCodes.concat(numbers_char_codes);
+  if (includeSymbols) charCodes = charCodes.concat(symbols_char_codes);
+
+  const passwordCharacters = [];
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode =
+      charCodes[Math.floor(Math.random() * charCodes.length)];
+    passwordCharacters.push(String.fromCharCode(characterCode));
+  }
+  return passwordCharacters.join("");
+}
+
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
 
 function syncCharacterAmount(e) {
   const value = e.target.value;
